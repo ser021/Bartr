26d39b567060b2abf4ceadc9686eab2fc79a3266
Renee Lopez <rdlopez693@gmail.com>
2025-03-23 22:11:00 -0400

    Update home.php

--- phpTest/home.php
+++ phpTest/home.php
@@ -1,41 +1,51 @@
-<?php
-session_start();
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit();
-}
-?>
-
+<?php require "functions.php" ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="Style.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <!--Header Container-->
-    <div style="width: 100%; height: 91px;">
-        <div style="width: 100%; height: 90px; left: 0px; top: 1px; position: absolute; border-bottom: 1px #FFE599 solid"></div>
-        <!--Profile Button-->
-        <button class="login_button" onclick="goToProfile()" style="right: 200px; top:19px; position: absolute;">Profile</button>
-        <!--Messages Button-->
-        <button class="login_button" onclick="goToMessages()" style="right: 37px; top:19px; position: absolute;">Messages</button>
-        <!--Logo-->
-        <div style="width: 185px; height: 73px; left: 37px; top: 0px; position: absolute; color: #FFE599; font-size: 80px; font-family: Castoro; font-weight: 400; word-wrap: break-word">Bartr</div>
-    </div>
-    <!--Post Container-->
+    <nav>
+        <div class="logo">Bartr</div>
+        <div class="links">
+            <a href="home.php">Home</a>
+            <a href="profile.php">Profile</a>
+            <a href="messages.php">Messages</a>
+        </div>
+    </nav>
 
-    <script>
-        function goToProfile() {
-            // Navigate to the login page
-            window.location.href = "profile.php";
-        }
-        function goToMessages() {
-            // Navigate to Registration
-            window.location.href = "messages.php"
-        }
-    </script>
+    <main>
+        <div class="postholder">
+            <!-- PHP that connects to the database and gets 8 distinct posts -->
+            <?php $listings = getListings(8) ?>
+                <!-- PHP loop that separates the array into different posts for each row -->
+                <?php
+	                foreach ($listings as $listing) {
+                        ?>
+            <div class="postgrid" width="385px">
+                <div class="listingimage" style = "border-top-left-radius: 8px; border-top-right-radius: 8px;">
+                    <!-- Turns into image path variable from listings table -->
+                    <img width="385px" height="275px" src="<?php echo "{$listing['images']}" ?>" alt="" style="object-fit:cover;">
+                </div>
+                <div class="profilepic">
+                    <!-- STILL NEED TO DO Turns into image path variable from profile table -->
+                    <img width="82px" height="82px" src="profilepictures/profile pic 1.jpg" alt="" style="object-fit:cover;">
+                </div>
+                <div class="title">
+                    <!-- Turns into variable for title from listings table -->
+                    <?php echo($listing['title'])?>
+                </div>
+                <div class="description">
+                    <!-- Turns into variable for description from listings table -->
+                    <?php echo($listing['description'])?>
+                </div>
+            </div>
+        <?php } ?><br>
+        </div>
+    </main>
+
 </body>
 </html>
